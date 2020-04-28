fd9ae36f8d571c00f3c30f5022dde48883dd7528
cgraves09 <christopher.graves09@gmail.com>
2020-04-27 20:44:40 -0400

    server code

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //  Data
-
-
-
+let reservation = [];
+let waitList = [];
 // Routes
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname, 'home.html'))
@@ -25,6 +24,30 @@ app.get('/reserve', function(req,res){
   res.sendFile(path.join(__dirname, 'reserve.html'));
 });
 
+app.get('/api/tables',function(req,res){
+  return res.json(reservation, waitlist)
+});
+
+app.post("/api/reservations", function(req, res) {
+  let newReservation = req.body;
+  if(reservation.length >= 5){
+    app.post("/api/waitingList", function(req, res){
+
+      waitList.push(newReservation);
+      res.json(newReservation);
+    })
+    
+  }
+  else{
+    reservation.push(newReservation);
+    res.json(newReservation);
+  }
+  
+ 
+});
+
+
+// APP Listening
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
